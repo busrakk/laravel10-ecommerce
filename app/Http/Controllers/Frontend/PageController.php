52f92b8c6d68913856c3b7d16e95cefeee28a444
busrakk <busrakartal160@gmail.com>
2023-08-13 20:36:20 +0300

    page creation

---
+++ app/Http/Controllers/Frontend/PageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function contact(){
+        return view('frontend.pages.contact');
+    }
+
+    public function about(){
+        return view('frontend.pages.about');
+    }
+
+    public function product(){
+        return view('frontend.pages.products');
+    }
+
+    public function productdetail(){
+        return view('frontend.pages.product');
+    }
+
+    public function cart(){
+        return view('frontend.pages.cart');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\PageHomeController;
+use App\Http\Controllers\Frontend\PageController;
+use App\Http\Controllers\Frontend\PageHomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PageHomeController::class, 'index'])->name('index');
+Route::get('/about', [PageController::class, 'about'])->name('about');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact');
+Route::get('/product', [PageController::class, 'product'])->name('product');
+Route::get('/product/detail', [PageController::class, 'productdetail'])->name('productdetail');
+Route::get('/cart', [PageController::class, 'cart'])->name('cart');
+
+
